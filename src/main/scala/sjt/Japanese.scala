801d81a493c9dcd477b9df58d378b4ab2dad8859
Danilo Salvio <danilo_salvio@live.com>
2017-06-21 13:56:00 -0300

    Adds a type class instance for Char

--- src/main/scala/sjt/Japanese.scala
+++ src/main/scala/sjt/Japanese.scala
@@ -1,11 +1,31 @@
 package sjt
 
+import java.nio.charset.Charset
+
 trait Japanese[A] {
   def isHiragana(value: A): Boolean
+  def isHalfWidthKatakana(value: A):Boolean
+  def isFullWidthKatakana(value: A):Boolean
+  def isKatakana(value: A): Boolean = isFullWidthKatakana(value) || isHalfWidthKatakana(value)
+  def isKana(value: A): Boolean = isHiragana(value) || isKatakana(value)
+  def isKanji(value: A): Boolean
+  def containsHiragana(value: A): Boolean
+  def containsKatakana(value: A): Boolean
+  def containsKana(value: A): Boolean = containsHiragana(value) || containsKatakana(value)
+  def containsKanji(value: A): Boolean
+  def containsJapanese(value: A): Boolean = containsHiragana(value) || containsKatakana(value)
+  def isLatin(value: A): Boolean
 }
 object JapaneseInstances{
   implicit val japaneseChar = new Japanese[Char] {
     def isHiragana(value: Char): Boolean = ('\u3041' <= value) && (value <= '\u309e')
+    def isHalfWidthKatakana(value: Char):Boolean = ('\uff66' <= value) && (value <= '\uff9d')
+    def isFullWidthKatakana(value: Char):Boolean = ('\u30a1' <= value) && (value <= '\u30fe')
+    def isKanji(value: Char): Boolean = (('\u4e00' <= value) && (value <= '\u9fa5')) || (('\u3005' <= value) && (value <= '\u3007'))
+    def containsHiragana(value: Char): Boolean = isHiragana(value)
+    def containsKatakana(value: Char): Boolean = isKatakana(value)
+    def containsKanji(value: Char): Boolean = isKanji(value)
+    def isLatin(value: Char): Boolean = Charset.forName("US-ASCII").newEncoder().canEncode(value)
   }
 }
 object Japanese{
