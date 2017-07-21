6b77ea3e83d820632a1ce72005a6c6f24b9045e2
Danilo Salvio <danilo_salvio@live.com>
2017-07-21 00:29:00 -0300

    Finishes kanji spec tests

--- src/test/scala/KanjiTransliteration.scala
+++ src/test/scala/KanjiTransliteration.scala
@@ -14,9 +14,9 @@ class KanjiTransliteration extends FlatSpec with Matchers{
 
   "Kanji" should "correctly transliterate to romaji" in {
     sentenceA.toRomaji() should be("o sushi ga tabe tai")
-    //sentenceB.toRomaji() should be("shinsatsu itte ki mashi ta. saishū mokuhyō wa shinzō ishoku toyuu no wa kawara nai sō de, jibun no shinzō ja iki rare nai mitai desu. okane mo kakarushi, shujutsu suru no mo nyūin suru no mo mō iya da na ～… nante koto wo kangaenagara vita de oboro mura sei wo purei shite mashi ta! 　 ushi oni to uma oni tte bosu yori tsuyoku nai!? 　 zenzen kate nai n dakedo …")
-    //sentenceC.toRomaji() should be("「 ima 」 okite iru koto wo mitsukeyo u. kokunai no nyūsu kara mijika na deki goto made, minna no wadai ga wakaru 「 ima 」 okite iru koto wo mitsukeyo u. kokunai no nyūsu kara mijika na deki goto made, minna no wadai ga wakaru")
-    //sentenceD.toRomaji() should be("minasan wa nippon no yottsu no ōkina shima no namae wo shitte i masu ka. nippon ni wa tōkyō no yō na, sekai ni yoku shira rete iru toshi ga takusan ari masuga, minasan wa donna toshi namae wo kiki ta koto ga ari masu ka.")
+    sentenceB.toRomaji() should be("shinsatsu itte ki mashi ta. saishū mokuhyō wa shinzō ishoku toyuu no wa kawara nai sō de, jibun no shinzō ja iki rare nai mitai desu. okane mo kakarushi, shujutsu suru no mo nyūin suru no mo mō iya da na * nante koto wo kangaenagara * de oboro mura sei wo * shite mashi ta! 　 ushi oni to uma oni tte bosu yori tsuyoku nai!? 　 zenzen kate nai n dakedo …")
+    sentenceC.toRomaji() should be("「 ima 」 okite iru koto wo mitsukeyo u. kokunai no nyūsu kara mijika na deki goto made, minna no wadai ga wakaru 「 ima 」 okite iru koto wo mitsukeyo u. kokunai no nyūsu kara mijika na deki goto made, minna no wadai ga wakaru")
+    sentenceD.toRomaji() should be("minasan wa nippon no yottsu no ōkina shima no namae wo shitte i masu ka. nippon ni wa tōkyō no yō na, sekai ni yoku shira rete iru toshi ga takusan ari masuga, minasan wa donna toshi namae wo kiki ta koto ga ari masu ka.")
     sentenceE.toRomaji() should be("kiku")
     sentenceF.toRomaji() should be("ōkina")
     sentenceG.toRomaji() should be("tōkyō")
@@ -24,9 +24,9 @@ class KanjiTransliteration extends FlatSpec with Matchers{
 
   it should "correctly transliterate to hiragana" in {
     sentenceA.toHiragana() should be("お すし が たべ たい")
-    //sentenceB.toHiragana() should be("さ")
-    //sentenceC.toHiragana() should be("さ")
-    //sentenceD.toHiragana() should be("さ")
+    sentenceB.toHiragana() should be("しんさついってきました。さいしゅうもくひょうはしんぞういちょくというのはかわらないそうで、ジぶんのしんぞうぢゃいきられないみたいです。おかねもかかるし、しゅジュつするのもにゅういんするのももういやだな*なんてことをかんがえながら*でおぼろむらせいを*してました！　うしおにとうまおにってぼすよりつよくない！？　ぜんぜんかてないんだけど…")
+    sentenceC.toHiragana() should be("「いま」おきていることをみつけよう。こくないのにゅうすからみぢかなできごとまで、みんなのわだいがわかる「いま」おきていることをみつけよう。こくないのにゅうすからみぢかなできごとまで、みんなのわだいがわかる")
+    sentenceD.toHiragana() should be("みなさんはにっぽんのよっつのおおきなしまのなまえをしっていますか。にっぽんにはとうきょうのような、せかいによくしられているとしがたくさんありますが、みなさんはどんなとしなまえをききたことがありますか。")
     sentenceE.toHiragana() should be("きく")
     sentenceF.toHiragana() should be("おうきな")
     sentenceG.toHiragana() should be("とうきょう")
@@ -34,9 +34,9 @@ class KanjiTransliteration extends FlatSpec with Matchers{
 
   it should "correctly transliterate to katakana" in {
     sentenceA.toKatakana() should be("オ スシ ガ タベ タイ")
-    //sentenceB.toKatakana() should be("さ")
-    //sentenceC.toKatakana() should be("さ")
-    //sentenceD.toKatakana() should be("さ")
+    sentenceB.toKatakana() should be("シンサツ・イッ・テ・キ・マシ・タ。・サイシュウ・モクヒョウ・ハ・シンゾウ・イショク・トイウ・ノ・ハ・カワラ・ナイ・ソウ・デ、・ジブン・ノ・シンゾウ・ジャ・イキ・ラレ・ナイ・ミタイ・デス。・オカネ・モ・カカル・シ、・シュジュツ・スル・ノ・モ・ニュウイン・スル・ノ・モ・モウ・イヤ・ダ・ナ・*・ナンテ・コト・ヲ・カンガエ・ナガラ・*・デ・オボロ・ムラ・セイ・ヲ・*・シ・テ・マシ・タ！・　・ウシ・オニ・ト・ウマ・オニ・ッテ・ボス・ヨリ・ツヨク・ナイ！？・　・ゼンゼン・カテ・ナイ・ン・ダ・ケド・…")
+    sentenceC.toKatakana() should be("「・イマ・」・オキ・テ・イル・コト・ヲ・ミツケヨ・ウ。・コクナイ・ノ・ニュース・カラ・ミヂカ・ナ・デキ・ゴト・マデ、・ミンナ・ノ・ワダイ・ガ・ワカル・「・イマ・」・オキ・テ・イル・コト・ヲ・ミツケヨ・ウ。・コクナイ・ノ・ニュース・カラ・ミヂカ・ナ・デキ・ゴト・マデ、・ミンナ・ノ・ワダイ・ガ・ワカル")
+    sentenceD.toKatakana() should be("ミナサン・ハ・ニッポン・ノ・ヨッツ・ノ・オオキナ・シマ・ノ・ナマエ・ヲ・シッ・テ・イ・マス・カ。・ニッポン・ニ・ハ・トウキョウ・ノ・ヨウ・ナ、・セカイ・ニ・ヨク・シラ・レ・テ・イル・トシ・ガ・タクサン・アリ・マス・ガ、・ミナサン・ハ・ドンナ・トシ・ナマエ・ヲ・キキ・タ・コト・ガ・アリ・マス・カ。")
     sentenceE.toKatakana() should be("キク")
     sentenceF.toKatakana() should be("オーキナ")
     sentenceG.toKatakana() should be("トーキョー")
